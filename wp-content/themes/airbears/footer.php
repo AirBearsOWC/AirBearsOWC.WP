9b3f12dca6cedb1ce50fbdb84c438da695ada98b
RobertAKARobin <robertgfthomas@gmail.com>
2017-04-29 08:35:59 -0500

    Added footer menu

--- wp-content/themes/airbears/footer.php
+++ wp-content/themes/airbears/footer.php
@@ -1,2 +1,10 @@
+<?php $menu_items = wp_get_nav_menu_items(get_nav_menu_locations()['footer-bar']); ?>
 
-<footer>This is the footer.</footer>
+<footer>
+	<p>This is the footer.</p>
+	<ul>
+<?php foreach($menu_items as $item): ?>
+		<li><a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a></li>
+<?php endforeach; ?>
+	</ul>
+</footer>
--- wp-content/themes/airbears/functions.php
+++ wp-content/themes/airbears/functions.php
@@ -1,8 +1,9 @@
 <?php
 
-function add_nav_bar(){
+function add_menus(){
 	register_nav_menu('nav-bar', 'Navigation bar');
+	register_nav_menu('footer-bar', 'Footer bar');
 }
-add_action('init', 'add_nav_bar');
+add_action('init', 'add_menus');
 
 ?>
